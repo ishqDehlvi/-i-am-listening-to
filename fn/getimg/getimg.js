2aba92d6c21caec5847c85be5ad7d6d45f0ded65
Inzemam ul-Haq <Inzemamhaq05@gmail.com>
2023-01-23 12:17:34 +0000

    first commit

---
+++ fn/getimg/getimg.js
@@ -0,0 +1,31 @@
+const { Octokit } = require("@octokit/core");
+
+const gtoken = process.env.G_TOKEN;
+// github personal access token - https://github.com/settings/tokens/new?scopes=repo
+
+const octokit = new Octokit({ auth: gtoken });
+
+const handler = async (event) => {
+  try {
+    // extract SVG info from the image
+    const fileInfo = await octokit.request(
+      "GET /repos/{owner}/{repo}/contents/{path}",
+      {
+        owner: "ishqdehlvi",
+        repo: "ishqdehlvi",
+        path: "music-badge.svg",
+      }
+    );
+
+    return {
+      statusCode: 200,
+      body: fileInfo.data.content,
+      headers: { "content-type": "image/svg+xml" },
+      isBase64Encoded: true,
+    };
+  } catch (error) {
+    return { statusCode: 500, body: error.toString() };
+  }
+};
+
+module.exports = { handler };
